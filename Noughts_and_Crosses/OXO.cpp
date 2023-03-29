c175ded0e7044770d46935a2f7c2d30643b39178
Professional <Professional@USER-PC>
2023-03-29 15:37:02 +0900

    DONE

--- Noughts_and_Crosses/OXO.cpp
+++ Noughts_and_Crosses/OXO.cpp
@@ -22,26 +22,33 @@ using namespace std;
 	
 const int SIZE = 3;
 
-void Fill_Grid(char grid[SIZE][SIZE], int SIZE, int trg_i, int trg_j);
-void Print_Grid(char grid[SIZE][SIZE], int SIZE);
-void Check_Move_Target(char grid[SIZE][SIZE], int SIZE, int* ptrg_i, int* ptrg_j, int control);
-int Set_Symbol(char grid[SIZE][SIZE], int SIZE, int *ptrg_i, int *ptrg_j, char m_plr);
+void Fill_Grid(char grid[SIZE][SIZE], int SIZE, int trg_i, int trg_j); //Функция заполнения сетки
+void Print_Grid(char grid[SIZE][SIZE], int SIZE); //Функция вывода сетки
+void Check_Move_Target(char grid[SIZE][SIZE], int SIZE, int* ptrg_i, int* ptrg_j, int control); // Функция проверки движения курсора
+int Set_Symbol(char grid[SIZE][SIZE], int SIZE, int *ptrg_i, int *ptrg_j, char m_plr); //Функция установки символа игрока
+int Set_Symbol(char grid[SIZE][SIZE], int SIZE, int* ptrg_i, int* ptrg_j, int control);
+int Check_Win(char grid[SIZE][SIZE], int SIZE); // Функция проверки на победу
 
 void main()
 {
-	//setlocale(LC_ALL, "ru");
-	int control;
+
+	int control, trg_i = 0, trg_j = 0;
 	char move_player = X;
-	int trg_i = 0, trg_j = 0;
-	int* ptrg_i, * ptrg_j;
+	char grid[SIZE][SIZE];
+
+	int *ptrg_i, *ptrg_j;
+
 	ptrg_i = &trg_i;
 	ptrg_j = &trg_j;
-	char grid[SIZE][SIZE];
 
 	Fill_Grid(grid, SIZE, trg_i, trg_j);
 	Print_Grid(grid, SIZE);
 
-	do //цикл отлова событий
+	setlocale(LC_ALL, "ru");
+	cout << "\nУправление: стрелочками выбирается позиция курсора(*), Enter устанавливает символ игрока, ESC - выход" << endl;
+	setlocale(LC_ALL, "С");
+
+	do //Цикл отлова событий
 	{
 		control = _getch();
 		switch (control)
@@ -50,7 +57,7 @@ void main()
 		case MOVE_DOWN: Check_Move_Target(grid, SIZE, ptrg_i, ptrg_j, control); Fill_Grid(grid, SIZE, trg_i, trg_j); Print_Grid(grid, SIZE); break;
 		case MOVE_RIGHT: Check_Move_Target(grid, SIZE, ptrg_i, ptrg_j, control); Fill_Grid(grid, SIZE, trg_i, trg_j); Print_Grid(grid, SIZE); break;
 		case MOVE_LEFT: Check_Move_Target(grid, SIZE, ptrg_i, ptrg_j, control); Fill_Grid(grid, SIZE, trg_i, trg_j); Print_Grid(grid, SIZE); break;
-		case ENTER: Set_Symbol(grid, SIZE, ptrg_i, ptrg_j, move_player); Print_Grid(grid, SIZE); move_player == X ? move_player = O : move_player = X; break;
+		case ENTER: Set_Symbol(grid, SIZE, ptrg_i, ptrg_j, move_player); Print_Grid(grid, SIZE); Check_Win(grid, SIZE); move_player == X ? move_player = O : move_player = X; break;
 		}
 	} while (control != ESC);
 }
@@ -92,68 +99,35 @@ void Check_Move_Target(char grid[SIZE][SIZE], int SIZE, int* ptrg_i, int* ptrg_j
 {
 	if (control == MOVE_UP)
 	{
-		if (*ptrg_i == 2 && grid[(*ptrg_i) - 1][*ptrg_j] !=X && grid[(*ptrg_i) - 1][*ptrg_j] != O)
-		{
-			(*ptrg_i)--;
-		}
-		else if (*ptrg_i == 2 && grid[(*ptrg_i) - 2][*ptrg_j] != X && grid[(*ptrg_i) - 2][*ptrg_j] != O)
-		{
-			(*ptrg_i) -= 2;
-		}
-		else if(*ptrg_i == 1 && grid[(*ptrg_i) - 1][*ptrg_j] != X && grid[(*ptrg_i) - 1][*ptrg_j] != O)
-		{
-			(*ptrg_i)--;
-		}
+		if (*ptrg_i == 2 && grid[(*ptrg_i) - 1][*ptrg_j] !=X && grid[(*ptrg_i) - 1][*ptrg_j] != O) (*ptrg_i)--;
+		else if (*ptrg_i == 2 && grid[(*ptrg_i) - 2][*ptrg_j] != X && grid[(*ptrg_i) - 2][*ptrg_j] != O) (*ptrg_i) -= 2;
+		else if(*ptrg_i == 1 && grid[(*ptrg_i) - 1][*ptrg_j] != X && grid[(*ptrg_i) - 1][*ptrg_j] != O) (*ptrg_i)--;
+		else Set_Symbol(grid, SIZE, ptrg_i, ptrg_j, control);
 	}
 	else if (control == MOVE_DOWN)
 	{
-		if (*ptrg_i == 0 && grid[(*ptrg_i) + 1][*ptrg_j] != X && grid[(*ptrg_i) + 1][*ptrg_j] != O)
-		{
-			(*ptrg_i)++;
-		}
-		else if (*ptrg_i == 0 && grid[(*ptrg_i) + 2][*ptrg_j] != X && grid[(*ptrg_i) + 2][*ptrg_j] != O)
-		{
-			(*ptrg_i) += 2;
-		}
-		else if (*ptrg_i == 1 && grid[(*ptrg_i) + 1][*ptrg_j] != X && grid[(*ptrg_i) + 1][*ptrg_j] != O)
-		{
-			(*ptrg_i)++;
-		}
+		if (*ptrg_i == 0 && grid[(*ptrg_i) + 1][*ptrg_j] != X && grid[(*ptrg_i) + 1][*ptrg_j] != O) (*ptrg_i)++;
+		else if (*ptrg_i == 0 && grid[(*ptrg_i) + 2][*ptrg_j] != X && grid[(*ptrg_i) + 2][*ptrg_j] != O) (*ptrg_i) += 2;
+		else if (*ptrg_i == 1 && grid[(*ptrg_i) + 1][*ptrg_j] != X && grid[(*ptrg_i) + 1][*ptrg_j] != O) (*ptrg_i)++;
+		else Set_Symbol(grid, SIZE, ptrg_i, ptrg_j, control);
 	}
 	else if (control == MOVE_RIGHT)
 	{
-		if (*ptrg_j == 0 && grid[*ptrg_i][(*ptrg_j) + 1] != X && grid[*ptrg_i][(*ptrg_j) + 1] != O)
-		{
-			(*ptrg_j)++;
-		}
-		else if (*ptrg_j == 0 && grid[*ptrg_i][(*ptrg_j) + 2] != X && grid[*ptrg_i][(*ptrg_j) + 2] != O)
-		{
-			(*ptrg_j) += 2;
-		}
-		else if (*ptrg_j == 1 && grid[*ptrg_i][(*ptrg_j) + 1] != X && grid[*ptrg_i][(*ptrg_j) + 1] != O)
-		{
-			(*ptrg_j)++;
-		}
+		if (*ptrg_j == 0 && grid[*ptrg_i][(*ptrg_j) + 1] != X && grid[*ptrg_i][(*ptrg_j) + 1] != O) (*ptrg_j)++;
+		else if (*ptrg_j == 0 && grid[*ptrg_i][(*ptrg_j) + 2] != X && grid[*ptrg_i][(*ptrg_j) + 2] != O) (*ptrg_j) += 2;
+		else if (*ptrg_j == 1 && grid[*ptrg_i][(*ptrg_j) + 1] != X && grid[*ptrg_i][(*ptrg_j) + 1] != O) (*ptrg_j)++;
+		else Set_Symbol(grid, SIZE, ptrg_i, ptrg_j, control);		
 	}
 	else if (control == MOVE_LEFT && *ptrg_j != 0)
 	{
-		if (*ptrg_j == 2 && grid[*ptrg_i][(*ptrg_j) - 1] != X && grid[*ptrg_i][(*ptrg_j) - 1] != O)
-		{
-			(*ptrg_j)--;
-		}
-		else if (*ptrg_j == 2 && grid[*ptrg_i][(*ptrg_j) - 2] != X && grid[*ptrg_i][(*ptrg_j) - 2] != O)
-		{
-			(*ptrg_j) -= 2;
-		}
-		else if (*ptrg_j == 1 && grid[*ptrg_i][(*ptrg_j) - 1] != X && grid[*ptrg_i][(*ptrg_j) - 1] != O)
-		{
-			(*ptrg_j)--;
-		}
+		if (*ptrg_j == 2 && grid[*ptrg_i][(*ptrg_j) - 1] != X && grid[*ptrg_i][(*ptrg_j) - 1] != O) (*ptrg_j)--;
+		else if (*ptrg_j == 2 && grid[*ptrg_i][(*ptrg_j) - 2] != X && grid[*ptrg_i][(*ptrg_j) - 2] != O) (*ptrg_j) -= 2;
+		else if (*ptrg_j == 1 && grid[*ptrg_i][(*ptrg_j) - 1] != X && grid[*ptrg_i][(*ptrg_j) - 1] != O) (*ptrg_j)--;
+		else Set_Symbol(grid, SIZE, ptrg_i, ptrg_j, control);
 	}
-
 }
 
-int Set_Symbol(char grid[SIZE][SIZE], int SIZE, int *ptrg_i, int *ptrg_j, char m_plr)
+int Set_Symbol(char grid[SIZE][SIZE], int SIZE, int* ptrg_i, int* ptrg_j, char m_plr)
 {
 	grid[*ptrg_i][*ptrg_j] = m_plr;
 
@@ -170,4 +144,60 @@ int Set_Symbol(char grid[SIZE][SIZE], int SIZE, int *ptrg_i, int *ptrg_j, char m
 			}
 		}
 	}	
+}
+
+int Set_Symbol(char grid[SIZE][SIZE], int SIZE, int* ptrg_i, int* ptrg_j, int control)
+{
+	for (int i = 0; i < SIZE; i++)
+	{
+		for (int j = 0; j < SIZE; j++)
+		{
+			if (grid[i][j] == EMPTY)
+			{
+				grid[i][j] = TARGET;
+				*ptrg_i = i;
+				*ptrg_j = j;
+				return 0;
+			}
+		}
+	}
+}
+
+int Check_Win(char grid[SIZE][SIZE], int SIZE)
+{
+	if (grid[1][1] != EMPTY && ((grid[1][1] == grid[0][0] && grid[1][1] == grid[2][2]) || (grid[1][1] == grid[0][2] && grid[1][1] == grid[2][0]) || (grid[1][1] == grid[0][1] && grid[1][1] == grid[2][1]) || (grid[1][1] == grid[1][0] && grid[1][1] == grid[1][2]))) //Проверка от центра
+	{
+		Print_Grid(grid, SIZE);
+		setlocale(LC_ALL, "ru");
+		cout << "Победил игрок " << grid[1][1] << "!" << endl;
+		setlocale(LC_ALL, "С");
+	}	
+	else if (grid[0][0] != EMPTY && ((grid[0][0] == grid[0][1] && grid[0][0] == grid[0][2]) || (grid[0][0] == grid[1][0] && grid[0][0] == grid[2][0]))) //Проверка от верхнего левого угла
+	{
+		Print_Grid(grid, SIZE);
+		setlocale(LC_ALL, "ru");
+		cout << "Победил игрок " << grid[0][0] << "!" << endl;
+		setlocale(LC_ALL, "С");
+	}
+	else if (grid[2][2] != EMPTY && ((grid[2][2] == grid[2][1] && grid[2][2] == grid[2][0]) || (grid[2][2] == grid[1][2] && grid[2][2] == grid[0][2]))) //Проверка от нихнего правого угла
+	{
+		Print_Grid(grid, SIZE);
+		setlocale(LC_ALL, "ru");
+		cout << "Победил игрок " << grid[2][2] << "!" << endl;
+		setlocale(LC_ALL, "С");
+	}
+	else //Проверка на ничью
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			for (int j = 0; j < SIZE; j++)
+			{
+				if (grid[i][j] == EMPTY) return 0;
+			}
+		}
+		setlocale(LC_ALL, "ru");
+		cout << "Ничья" << endl;
+		setlocale(LC_ALL, "С");
+		return 0;
+	}
 }
